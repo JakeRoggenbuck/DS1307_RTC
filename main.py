63bbd2b9719280fc437f9b4d8243413dde76e4eb
Jake <jakeroggenbuck2@gmail.com>
2022-11-19 17:31:56 -0800

    Read single byte

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from smbus2 import SMBus
 
 # Open the BUS number 1, i2c has multiple
 with SMBus(1) as BUS:
-    data = BUS.read_i2c_block_data(
+    data = BUS.read_byte_data(
         # addr found in datasheet
-        i2c_addr=80,
-        register=0,
-        length=16,
+        0x0,
+        0,
     )
     print(data)
